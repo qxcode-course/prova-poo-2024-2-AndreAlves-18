f33a27994b7d8db5f1d0e063b5e962a74c8dc9e7
Andre Alves <freitasalvesandre16@alu.ufc.br>
2024-12-05 14:50:57 -0300

    atividade teste

--- prova/uber/student.cpp
+++ prova/uber/student.cpp
@@ -1,23 +1,106 @@
 #include <iostream>
 #include "fn.hpp"
 
+class Pessoa{
+    std::string nome {};
+    int dinheiro;
+public:
+    Pessoa(std::string nome = "", int dinheiro = 0): nome(nome), dinheiro(dinheiro){}
+    std::string str(){
+        return fn::format("{}:{}", nome, dinheiro);
+    }
+    std::string getNome(){
+        return this->nome;
+    }
+    int getDinheiro(){
+        return this->dinheiro;
+    }
+    void setDinheiro(int valor){
+        dinheiro += valor;
+    }
+};
+
+class Moto{
+    int custo{0};
+    std::shared_ptr<Pessoa> motorista {nullptr};
+    std::shared_ptr<Pessoa> passageiro {nullptr};
+public:
+    Moto(){}
+    std::string str(){
+        if(motorista == nullptr && passageiro == nullptr){
+            return fn::format("Cost: {}, Driver: None, Passenger: None", custo);
+            
+        }
+        if(motorista == nullptr){
+            return fn::format("Cost: {}, Driver: None, Passenger: {}", custo, passageiro->str());
+            
+        }
+        if(passageiro == nullptr){
+            return fn::format("Cost: {}, Driver: {}, Passenger: None", custo, motorista->str());
+        }
+        return fn::format("Cost: {}, Driver: {}, Passenger: {}", custo, motorista->str(), passageiro->str());
+    }
+    void setMotorista(std::shared_ptr<Pessoa> motoris){
+        if(motorista == nullptr){
+            motorista = motoris;
+            return;
+        }
+    }
+    void setPassageiro(std::shared_ptr<Pessoa> pas){
+        if(passageiro == nullptr){
+            passageiro = pas;
+            return;
+        }
+    }
+    void dirigir(int valor){
+        custo += valor;
+    }
+    void descer(){
+        if(passageiro != nullptr){
+            if(custo > passageiro->getDinheiro()){
+                fn::write("fail: Passenger does not have enough money");
+                fn::print("{}:0 leave\n", passageiro->getNome());
+                motorista->setDinheiro(custo);
+                custo = 0;
+                passageiro = nullptr;
+                return;
+            }
+            fn::print("{}:{} leave\n", passageiro->getNome(), custo);
+            motorista->setDinheiro(custo);
+            custo = 0;
+            passageiro = nullptr;
+            return;
+        }
+    }
+
+};
+
 
 class Adapter {
+    Moto moto;
 public:
     void setDriver(std::string name, int money) {
         (void) name;
         (void) money;
+        auto person = std::make_shared<Pessoa>(name, money);
+        moto.setMotorista(person);
+
     }
     void setPass(std::string name, int money) {
         (void) name;
         (void) money;
+        auto person = std::make_shared<Pessoa>(name, money);
+        moto.setPassageiro(person);
     }
     void drive(int distance) {
         (void) distance;
+        moto.dirigir(distance);
     }
     void leavePass() {
+        moto.descer();
     }
     void show() {
+        std::cout << moto.str() << '\n';
     }
 };
 
